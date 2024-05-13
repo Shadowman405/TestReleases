11e1245fc532cf0818936edcb327aa6088c2ccde
Shadowman405 <maximus405@gmail.com>
2024-05-13 16:39:12 +0300

    new

---
+++ testFramework/testFramework/testFramework.h
@@ -0,0 +1,18 @@
+//
+//  testFramework.h
+//  testFramework
+//
+//  Created by Maxim Mitin on 13.05.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testFramework.
+FOUNDATION_EXPORT double testFrameworkVersionNumber;
+
+//! Project version string for testFramework.
+FOUNDATION_EXPORT const unsigned char testFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testFramework/PublicHeader.h>
+
+
